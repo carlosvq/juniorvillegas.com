ced59a92e501f67c795f37d5406e545bf3b3e93d
Carlos Valdez <carlos@carlosvq.com>
2024-05-14 12:01:11 -0400

    chore: add register link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify";
-
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  redirects: {
+    "/register": "https://app.formbricks.com/s/clw5sqnea02e0109cpehm035r",
+  },
+  integrations: [tailwind(), react({ include: ["**/react/*"] })],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
