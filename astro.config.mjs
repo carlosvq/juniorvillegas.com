0273a0bf1b2d74f7e2559251cfee2767793480fd
Carlos Valdez <carlos@carlosvq.com>
2024-07-04 10:08:03 -0400

    fix: prettier and eslint config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({ include: ["**/react/*"] })],
+  site: "https://theuq.com",
+  integrations: [mdx(), sitemap(), tailwind(), react({
+    include: ["**/react/*"]
+  })],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
